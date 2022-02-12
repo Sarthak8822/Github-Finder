b03732cb5204c611ea05944ac37c5514504528bf
Sarthak <sarthakpravin08@gmail.com>
2022-02-12 12:26:00 +0530

    final commit

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ export const About = () => {
     <Fragment>
         <h1>About This App</h1>
         <p>This is My 2nd React App and I made this while I'm learning Reactjs.
-        Wish me luck :p
+        Wish me luck ✌️
         </p>
         <i className="fa fa-hand-peace-o"></i>
     </Fragment>
